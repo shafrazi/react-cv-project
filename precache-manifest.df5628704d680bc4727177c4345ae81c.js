14855d78d5f924e81898ed3fc2ad66affe20f8c7
shafrazi <shafrazi@live.com>
2020-10-03 00:21:57 +0530

    Updates

---
+++ precache-manifest.df5628704d680bc4727177c4345ae81c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05310bca2ce66049ea2e3bdcc39be3fb",
+    "url": "/react-cv-project/index.html"
+  },
+  {
+    "revision": "5ae0c3f346218523f7b9",
+    "url": "/react-cv-project/static/css/main.a9e7205f.chunk.css"
+  },
+  {
+    "revision": "8370f2f33b6224e6c25d",
+    "url": "/react-cv-project/static/js/2.ca16e0b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cv-project/static/js/2.ca16e0b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae0c3f346218523f7b9",
+    "url": "/react-cv-project/static/js/main.d6f4cca3.chunk.js"
+  },
+  {
+    "revision": "0b1953340865661c025d",
+    "url": "/react-cv-project/static/js/runtime-main.8c985207.js"
+  }
+]);
